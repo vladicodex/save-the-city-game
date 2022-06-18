2a31bb116a2c094f84ee0b4380e9fecd6e26e241
Vladimir <vladimiremiliojc@outlook.com>
2022-06-18 16:54:34 -0400

    Adds js properties on canvas layout and getContext

--- js/main.js
+++ js/main.js
@@ -2,4 +2,21 @@ var bomb;
 
 function startGame() {
     bomb = new component(20, 50, "orange", 240, -50);
+    area.start();
+}
+
+var area = {
+    canvas : document.getElementById("canvas"),
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        //document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    stop : function() {
+        clearInterval(this.interval);
+    },    
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
