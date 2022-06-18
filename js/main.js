3c61ddfdf3a9701cfa3e4ba048e0de3a227248ea
Vladimir <vladimiremiliojc@outlook.com>
2022-06-18 16:56:57 -0400

    Adds components for bomb movement falling down

--- js/main.js
+++ js/main.js
@@ -20,3 +20,25 @@ var area = {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
+
+function component(width, height, color, x, y, type) {
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    this.speedX = 0;
+    this.speedY = 0;    
+    this.gravity = 0.05;
+    this.gravitySpeed = 0;
+    this.update = function() {
+        ctx = area.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.gravitySpeed += this.gravity;
+        this.x += this.speedX;
+        this.y += this.speedY + this.gravitySpeed;
+    }
+}
